61998ba90f4df65f42a1d149e65b08ce6086b166
vanipriya_piramanathan <vpriyaarun23@gmail.com>
2023-01-15 22:38:11 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express =require("express");
+const app=express();
+const fs=require("fs");
+const cors=require('cors');
+
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
+// app.use(cors({
+//   origin: 'http://localhost:3000'
+// }));
+
+const routes={
+    "/pharmacies":"returns pharmacies list for stratford", 
+  "/colleges" : "returns colleges list for stratford",
+"/doctors" : "returns doctors list for stratford ", 
+"/hospitals": "returns hospitals list for stratford" 
+}
+app.get("/",(req,res)=>{
+    res.send(routes)
+})
+
+app.get("/:city/:category",(req,res)=>{
+    const cityName=req.params.city.toLowerCase();
+    const category=req.params.category.toLowerCase();
+    let filePath=__dirname + '/data/' + cityName +'.json';
+    console.log(filePath);
+    const cityData=JSON.parse(fs.readFileSync(filePath,'utf-8'))
+    res.json(cityData[category]);
+})
+
+app.listen(PORT)
